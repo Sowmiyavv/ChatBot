8499e7892bbf087089ff71013aa73b7471d93327
Sowmiya V <sowmiyv@eu.trimblecorp.net>
2022-10-12 10:23:56 +0530

    added db

--- data_json.py
+++ data_json.py
@@ -1,128 +1,128 @@
-json_data_from_db = [
-  {
-    "id": "63442381ca58317016e57151",
-    "applicationName": "worksos",
-    "intents": [
-      {
-        "tag": "greeting",
-        "patterns": [
-          "Hi there",
-          "How are you",
-          "Is anyone there?",
-          "Hey",
-          "Hola",
-          "Hello",
-          "Good day"
-        ],
-        "responses": [
-          {
-            "responseText": "Hello, thanks for asking",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          },
-          {
-            "responseText": "Good to see you again",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          },
-          {
-            "responseText": "Hi there, how can I help?",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          }
-        ]
-      },
-      {
-        "tag": "goodbye",
-        "patterns": [
-          "Bye",
-          "See you later",
-          "Goodbye",
-          "Nice chatting to you, bye",
-          "Till next time"
-        ],
-        "responses": [
-          {
-            "responseText": "See you!",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          },
-          {
-            "responseText": "Have a nice day",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          },
-          {
-            "responseText": "Bye! Come back again soon.",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          }
-        ]
-      },
-      {
-        "tag": "options",
-        "patterns": [
-          "How you could help me?",
-          "What you can do?",
-          "What help you provide?",
-          "How you can be helpful?",
-          "What support is offered"
-        ],
-        "responses": [
-          {
-            "responseText": "I am chatbot for worksos, I am here to help you",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          },
-          {
-            "responseText": "I am chatbot for worksos, I am here to help you",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          },
-          {
-            "responseText": "I am chatbot for worksos, I am here to help you",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          }
-        ]
-      },
-      {
-        "tag": "worksos",
-        "patterns": [
-          "what is worksos"
-        ],
-        "responses": [
-          {
-            "responseText": "Its CCS product",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          },
-          {
-            "responseText": "Its CCS product",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          }
-        ]
-      },
-      {
-        "tag": "worksManager",
-        "patterns": [
-          "what is worksManager"
-        ],
-        "responses": [
-          {
-            "responseText": "Its CCS product for devices and dozers",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          },
-          {
-            "responseText": "Its CCS product for devices and dozers",
-            "helpLink": None,
-            "apiComputeUrlAndHelpLink": None
-          }
-        ]
-      }
-    ]
-  }
-]
-
+# json_data_from_db = [
+#   {
+#     "id": "63442381ca58317016e57151",
+#     "applicationName": "worksos",
+#     "intents": [
+#       {
+#         "tag": "greeting",
+#         "patterns": [
+#           "Hi there",
+#           "How are you",
+#           "Is anyone there?",
+#           "Hey",
+#           "Hola",
+#           "Hello",
+#           "Good day"
+#         ],
+#         "responses": [
+#           {
+#             "responseText": "Hello, thanks for asking",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           },
+#           {
+#             "responseText": "Good to see you again",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           },
+#           {
+#             "responseText": "Hi there, how can I help?",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           }
+#         ]
+#       },
+#       {
+#         "tag": "goodbye",
+#         "patterns": [
+#           "Bye",
+#           "See you later",
+#           "Goodbye",
+#           "Nice chatting to you, bye",
+#           "Till next time"
+#         ],
+#         "responses": [
+#           {
+#             "responseText": "See you!",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           },
+#           {
+#             "responseText": "Have a nice day",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           },
+#           {
+#             "responseText": "Bye! Come back again soon.",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           }
+#         ]
+#       },
+#       {
+#         "tag": "options",
+#         "patterns": [
+#           "How you could help me?",
+#           "What you can do?",
+#           "What help you provide?",
+#           "How you can be helpful?",
+#           "What support is offered"
+#         ],
+#         "responses": [
+#           {
+#             "responseText": "I am chatbot for worksos, I am here to help you",
+#             # "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           },
+#           {
+#             "responseText": "I am chatbot for worksos, I am here to help you",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           },
+#           {
+#             "responseText": "I am chatbot for worksos, I am here to help you",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           }
+#         ]
+#       },
+#       {
+#         "tag": "worksos",
+#         "patterns": [
+#           "what is worksos"
+#         ],
+#         "responses": [
+#           {
+#             "responseText": "Its CCS product",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           },
+#           {
+#             "responseText": "Its CCS product",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           }
+#         ]
+#       },
+#       {
+#         "tag": "worksManager",
+#         "patterns": [
+#           "what is worksManager"
+#         ],
+#         "responses": [
+#           {
+#             "responseText": "Its CCS product for devices and dozers",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           },
+#           {
+#             "responseText": "Its CCS product for devices and dozers",
+#             "helpLink": None,
+#             "apiComputeUrlAndHelpLink": None
+#           }
+#         ]
+#       }
+#     ]
+#   }
+# ]
+#
